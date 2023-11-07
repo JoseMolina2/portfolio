2142906e3e3c75a77ed25136bdfd25a4450e2b44
JoseMolina2 <jmmwork@protonmail.com>
2023-11-07 00:17:15 -0600

    create layout structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="w3-light-grey">Hola</div>;
+  return (
+    <div className="w3-light-grey">
+      {/* Page Container */}
+      <div id="container" className="w3-content w3-margin-top">
+        {/* The Grid */}
+        <div className="w3-row-padding">
+          {/* Left Column */}
+          <div className="w3-third"></div>
+          <div className="w3-twothird"></div>
+        </div>
+        <footer className="w3-container w3-teal w3-center w3-margin-top"></footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
